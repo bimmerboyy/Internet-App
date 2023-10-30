003fc6276d503b3ba953cef7a4794fe50465d6a8
Tarik Kucevic <tarikkucevic@gmail.com>
2023-10-30 12:14:20 +0100

    introduction to php variables,strings etc

--- php-course/index.php
+++ php-course/index.php
@@ -8,26 +8,53 @@
 
 // echo $bilosta;
 
-$test = 'Hello World';
+// $test = 'Hello World';
 // $test = 'nesto drugo';
-$cijeli_broj = 11;
-$float_broj = 5.14;
-$tacno_netacno = true;
+// $cijeli_broj = 11;
+// $float_broj = 5.14;
+// $tacno_netacno = true;
+// $prazno = NULL;
 
-var_dump($test);
+// $loPta = 'okrugla';
+// $lopta = 'sssss';
 
-echo "<br>";
+// var_dump($lopta);
+// var_dump($loPta);
 
-var_dump($cijeli_broj);
+// var_dump($test);
 
-echo "<br>";
+// echo "<br>";
 
-var_dump($float_broj);
+// var_dump($cijeli_broj);
 
-echo "<br>";
+// echo "<br>";
 
-var_dump($tacno_netacno);
-// echo $test;
+// var_dump($float_broj);
+
+// echo "<br>";
+
+// var_dump($tacno_netacno);
+// // echo $test;
+// echo "<br>";
+
+// var_dump($prazno);
+
+
+$broj_1 = 11;
+$broj_2 = 5;
+$rezultat = $broj_1 * $broj_2;
+
+// echo "Broj 1: " . $broj_1;
+
+// echo "<br>";
+
+// echo "Broj 2: " . $broj_2;
+
+// echo "<br>";
+
+// echo 'Rezultat:' . $rezultat;
+
+echo "$broj_1 minus $broj_2 jednako je: $rezultat"; 
 ?>
 
 <!-- <h1>Ovo je neki nalov</h1> -->
