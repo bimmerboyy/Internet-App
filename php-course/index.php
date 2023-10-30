e5a5eee9be4e5eb4c50f99bc15536f5f0ba0399b
Tarik Kucevic <tarikkucevic@gmail.com>
2023-10-30 11:47:22 +0100

    introduction to php variables,strings etc

--- php-course/index.php
+++ php-course/index.php
@@ -1,3 +1,33 @@
 <?php
-echo 'OVO JE INDEX FAJL' 
+// echo 'Hello World!';
+// print("<br> Hello World");
+
+// $bilosta = 5;
+// echo $bilosta . "<br>";
+// $bilosta = 10;
+
+// echo $bilosta;
+
+$test = 'Hello World';
+// $test = 'nesto drugo';
+$cijeli_broj = 11;
+$float_broj = 5.14;
+$tacno_netacno = true;
+
+var_dump($test);
+
+echo "<br>";
+
+var_dump($cijeli_broj);
+
+echo "<br>";
+
+var_dump($float_broj);
+
+echo "<br>";
+
+var_dump($tacno_netacno);
+// echo $test;
 ?>
+
+<!-- <h1>Ovo je neki nalov</h1> -->
