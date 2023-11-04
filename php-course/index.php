43f5c353fb9518add58b1f22e689558dc8de3c45
Tarik Kucevic <tarikkucevic@gmail.com>
2023-11-04 12:53:06 +0100

    introduction to php variables,strings etc

--- php-course/index.php
+++ php-course/index.php
@@ -40,9 +40,9 @@
 // var_dump($prazno);
 
 
-$broj_1 = 11;
-$broj_2 = 5;
-$rezultat = $broj_1 * $broj_2;
+// $broj_1 = 11;
+// $broj_2 = 5;
+// $rezultat = $broj_1 * $broj_2;
 
 // echo "Broj 1: " . $broj_1;
 
@@ -82,19 +82,48 @@ za spajanje teksta*/
 // $pozicija = strpos($tekst, "World");
 // echo $pozicija;
 
-$tekst = "Lorem ipsum, or lipsum as it is omg sometimes known, is dummy text used in laying out print lol, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
+// $tekst = "Lorem ipsum, or lipsum as it is omg sometimes known, is dummy text used in laying out print lol, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
 
-    “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”
-    The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
+//     “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”
+//     The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
     
-    The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.";
-$omg_pos = strpos($tekst,"omg");
-$lol_pos = strpos($tekst,"lol");
+//     The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.";
+// $omg_pos = strpos($tekst,"omg");
+// $lol_pos = strpos($tekst,"lol");
 
-$finish_pos = $lol_pos - $omg_pos;
-$preview = substr($tekst,$omg_pos,$finish_pos);
+// $finish_pos = $lol_pos - $omg_pos;
+// $preview = substr($tekst,$omg_pos,$finish_pos);
 
-echo $preview . "...";
+// echo $tekst . "<br> <br>";
+
+// $preciscen_tekst = str_replace("omg","X",$tekst);
+
+// $preciscen_tekst = str_replace("lol","X",$preciscen_tekst);
+// echo $preciscen_tekst;
+
+// echo $preview . "...";
+
+//Primer za velika slova 
+
+// $tekst = "OvO je TrOl NEki";
+
+// $velika_slova = strtolower($tekst);
+
+// echo $velika_slova;
+
+//Funkcije za upit da li tekst sadrzi nesto i funkcija trim
+
+// $email = "   ovoje@neki.com   ";
+
+// $sadrzi = str_contains($email, "@");
+
+// $novi = trim($email);
+
+// var_dump($sadrzi);
+
+// var_dump($novi);
+
+// echo $sadrzi;
 ?>
 
 <!-- <h1>Ovo je neki nalov</h1> -->
