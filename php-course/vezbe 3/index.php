d67fccbae38028b49e6ae499ce8f888d9c71873a
Tarik Kucevic <tarikkucevic@gmail.com>
2023-11-04 18:52:57 +0100

    introduction to php variables,strings etc

---
+++ php-course/vezbe 3/index.php
@@ -0,0 +1,75 @@
+<?php
+
+//Nizovi
+//Indeksirani niz
+// $brojevi = array(10,20,30,40,50);
+
+//kraci nacin za pisanje niza
+// $brojevi = [10,20,30,40,50];
+
+// echo $brojevi[2];
+
+//Asocijativni niz
+
+// $osoba = [
+//     "ime" => "Marko",
+//     "prezime" => "Markovic",
+//     "godine"=> 18,
+//     "zanimanje" => "porgramer"
+// ];
+
+// var_dump($osoba);
+
+// echo $osoba["ime"];
+
+//Visedimenzionalni asocijativni niz
+
+// $gradovi = [
+//     "Srbija" => ["Beograd","Novi Pazar","Nis"],
+//     "Hrvatska" => ["Zagreb","Split","Rijeku"],
+//     "Crna Gora" => ["Podgorica","Niksic","Budva"],
+//     "Bosna i Hercegovina" => ["Sarajevo","Mostar","Travnik"],
+
+// ];
+
+// var_dump($gradovi);
+
+// echo $gradovi["Srbija"][1];
+
+//Operacije push i pop na niz
+// $brojevi = [2,4,5];
+
+// var_dump($brojevi);
+
+// $element = array_push($brojevi,10, 15);
+
+// $poslednji_element = array_pop($brojevi);
+
+// echo $poslednji_element;
+
+// var_dump($brojevi);
+// $broj_elemenata = count($brojevi);
+
+// echo $broj_elemenata;
+
+//Spajanje dva niza u jedan
+// $brojevi1 = [2,4,5];
+// $brojevi2 = [10,11,12];
+
+// $treci_niz = array_merge($brojevi1,$brojevi2);
+
+// var_dump($treci_niz);
+
+//Sortiranje niza
+
+// $brojevi = [5,3,1,4,2];
+
+// sort($brojevi); //rsort($brojevi); reverse sort
+
+// var_dump($brojevi);
+
+//Petlje
+
+
+
+?>
