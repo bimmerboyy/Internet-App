0c3d55a6dbd562ea7ded9c6ed5006d9f48e95cf7
Tarik Kucevic <tarikkucevic@gmail.com>
2023-11-04 14:30:45 +0100

    introduction to php variables,strings etc

--- php-course/vezbe 2/index.php
+++ php-course/vezbe 2/index.php
@@ -26,15 +26,116 @@
 //     echo "Broj jeste 10"; 
 // }
 
-$broj1 = 5;
-$broj2 = 5;
+//Poredjenje varijeabli sa == je poredjenje samo vrednosti tih varijabli a poredjenje sa === je poredjenje i varijabli i njihovih tipova
 
-if($broj1 === $broj2){
-    echo "Brojevi su jednaki";
-}
-else{
-    echo "Brojevi nisu jednaki"; 
-}
+// $broj1 = 5;
+// $broj2 = 5;
 
+// if($broj1 === $broj2){
+//     echo "Brojevi su jednaki";
+// }
+// else{
+//     echo "Brojevi nisu jednaki"; 
+// }
+
+//Provera izmedju 10 i 50
+// $broj = 30;
+
+// if($broj > 10 && $broj < 50){
+//     echo "Broj je izmedju 10 i 50";
+// }
+// else {
+//     echo "Broj nije izmedju 10 i 50";
+// }
+
+ //Primer za ili ispod nula stepeni zamzavanje a vise od 100 kljucanje
+// $broj = -5;
+
+// if($broj < 0 || $broj > 100){
+//     echo "Broj je manji od 0 ili veci od 100😂";
+// }
+
+//Pravimo dva uslova 
+
+// $broj = 41;
+
+// if( ($broj > 10 && $broj < 20) || ($broj > 40 && $broj < 50) ){
+//     echo "Broj je izmedju 10 i 20 ili izmdeju 40 i 50";
+// }
+
+//Kombinacija koriscenjem prethodnih uslova na primeru
+// $ime = "Marko";
+// $godine = 25;
+// $zanimanje = "Programer";
+
+// if($ime == "Marko" && $godine >= 18 && $zanimanje == "Programer"){
+//     echo "Marko je punoljetni programer";
+// }
+
+
+//Ispitujemo parnost u skraceni if else
+// $broj = 5;
+
+// $parnost = ($broj % 2 == 0) ? "Broj je paran" : "Broj je neparan";
+
+// echo $parnost;
+// if(){
+//     echo "Broj je paran";
+// }
+// else{
+//     echo "Broj je neparan";
+// }
+
+$broj = 5;
+
+
+//Pretvaranje if uslova u switch
+
+//if uslov
+// if($broj == 1){
+//     echo "jabuka";
+
+// }
+// else if($broj == 2){
+//     echo "banana";
+// }
+// else if($broj == 3){
+//     echo "kruska";
+// }
+// else if($broj == 4){
+//     echo "jagoda";
+// }
+// else{
+//     echo "nema voca";
+// }
+
+//switch uslov
+
+// switch($broj){
+//     case 1:
+//         echo "jabuka";
+//     break;
+//     case 2:
+//         echo "banana";
+//     break;
+//     case 3:
+//         echo "kruska";
+//     break;
+//     case 4:
+//         echo "jagoda";
+//     break;
+//     default:
+//     echo "nema voca";
+// }
+
+//match uslov
+
+// $voce = match($broj){
+//     1 => "jabuka",
+//     2=> "banana",
+//     3 => "kruska",
+//     4 => "jagoda",
+//     default => "nema voca"
+// };
 
 ?>
