49436a102fd3757fe687afddd8e7b8b986f15e71
Tarik Kucevic <tarikkucevic@gmail.com>
2023-11-21 21:59:59 +0100

    example 7 exercise 4

--- Vezbe 4/primer 7/index.php
+++ Vezbe 4/primer 7/index.php
@@ -46,6 +46,113 @@ function getLinkFromStudents($students){
 
 // echo getLinkFromStudents($students);
 
+//Ova funkcija dodaje studenta u niz students
+
+function addStudents(&$students,$ime,$indeks,$prosek){
+   $students[$indeks][$ime] = $prosek;
+}
+
+addStudents($students,"Sabit Kangal","3000","7.5");
+
+//Funkcija za ispisivanje svih studenata
+
+function printAllStudents(){
+    global $students;
+    foreach($students as $indeks => $imeArray){
+        foreach($imeArray as $ime => $grade){
+            echo "$indeks => [$ime => $grade]<br>";
+        }
+    }
+}
+// printAllStudents();
+
+//Funkcija za kupljenje podataka sa requesta i smestanje u niz
+//Moramo da posaljemo adresu niza kada vrsimo dodavanje
+
+function initStudents(&$students){
+    if(isset($_POST["indeks"]) && isset($_POST["ime"]) && isset($_POST["prosek"])){
+        $students[$_POST["indeks"]][$_POST["ime"]] = $_POST["prosek"];
+    }
+    else{
+        echo "nije uspeo da ih postavi<br>";
+    }
+    
+}
+initStudents($students);
+
+// printAllStudents();
+
+function displayStudents($students){
+    $imeStudenta = "";
+    $indeksStudenta = "";
+    $prosek = "";
+    $duzinaNiza = 0;
+    foreach($students as $indeks => $imeArray){
+        foreach($imeArray as $ime => $grade){
+            $imeStudenta .= $ime . "_";
+            $indeksStudenta .= $indeks . "_";
+            $prosek .= $grade . "_";
+            $duzinaNiza = sizeof($students) + sizeof($imeArray);
+        }
+    }
+    $imeStudenta = explode("_",$imeStudenta);
+    $indeksStudenta = explode("_",$indeksStudenta);
+    $prosek = explode("_",$prosek);
+    $string = "";
+    for($i = 0;$i < $duzinaNiza;$i++){
+        $string .= 
+    "<tr>
+     <td>$indeksStudenta[$i]</td>
+     <td>$imeStudenta[$i]</td>
+     <td>$prosek[$i]</td>
+    </tr>";
+    }
+    echo "
+    <table style='width:500px;'>
+    <tr>
+    <th>Indeks</th>
+    <th>Ime i prezime</th>
+    <th>Prosek</th>
+    </tr>
+    $string
+    </table>
+    ";
+    
+
+}
+
+
+
+function deleteStudent(&$students, $indeksToDelete) {
+    if (isset($students[$indeksToDelete])) {
+        unset($students[$indeksToDelete]);
+        echo "Student with index $indeksToDelete deleted successfully.";
+    } else {
+        echo "Student with index $indeksToDelete not found.";
+    }
+}
+
+deleteStudent($students,"1000");
+
+displayStudents($students);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+table,tr,td{
+    border: 1px solid black;
+    border-collapse:collapse;
+}
+</style>
+<body>
+    
+</body>
+</html>
 
