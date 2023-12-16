476cd38ddf6a6a3b94124bff3bb31e8bfc87aa53
Tarik Kucevic <tarikkucevic@gmail.com>
2023-12-16 20:32:35 +0100

    exercising for second colloquium

--- drugi-kol/vezbe 8/primer4.php
+++ drugi-kol/vezbe 8/primer4.php
@@ -2,8 +2,16 @@
 
 class Messagge{
     const MESSAGGE = "Ovo je neka poruka"; //da bi smo ispisali ovaku neku poruku ne stavljamo dolar sign
+
+    function ispisiPoruku(){
+        echo self::MESSAGGE;
+    }
 }
 
-echo Messagge::MESSAGGE;
+echo Messagge::MESSAGGE . "<br>";
+
+$poruka = new Messagge();
+$poruka->ispisiPoruku();
+
 
 ?>
